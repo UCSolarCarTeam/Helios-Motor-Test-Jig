17aa4794c23a60892b286e1e4087fc5bcc4489f5
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2024-11-06 17:13:42 -0700

    ioc

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -433,9 +433,9 @@ static void MX_TIM7_Init(void)
 
   /* USER CODE END TIM7_Init 1 */
   htim7.Instance = TIM7;
-  htim7.Init.Prescaler = 320;
+  htim7.Init.Prescaler = 319;
   htim7.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim7.Init.Period = 9999;
+  htim7.Init.Period = 999;
   htim7.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim7) != HAL_OK)
   {
